8e4f408b5e17691eb66a4c6b8d5bf623f0ed2d2e
eshginfarzali <85007480+eshginfarzali@users.noreply.github.com>
2022-12-29 16:46:55 +0400

    data-types-learn

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,1 +1,36 @@
 console.log(`Hello to the world from Eshgin!`)
+
+console.log( typeof 'Eshgin') //string
+console.log( typeof 5) //number
+console.log( typeof 5.5) //number
+console.log( typeof true) //boolean
+console.log( typeof null) //object
+console.log( typeof undefined) //undefined
+
+
+// Declaring different variables of different data types
+
+let firstName = 'Eshgin' // first name of a person
+let lastName = 'Farzali' // last name of a person
+let country = 'Azerbaijan' // country
+let city = 'Baku' // capital city
+let age = 27 // age in years
+let isMarried = true 
+console.log(firstName, lastName, country, city, age, isMarried)
+
+
+
+// Declaring variables with number values
+let ageYears= 100 // age in years
+const gravity = 9.81 // earth gravity  in m/s2
+const boilingPoint = 100 // water boiling point, temperature in °C
+const PI = 3.14 // geometrical constant
+console.log(gravity, boilingPoint, PI)
+
+
+
+// Variables can also be declaring in one line separated by comma, however I recommend to use a seperate line to make code more readble
+let name_1= 'Eshgin', 
+    job = 'developer', 
+    live = 'Baku'
+console.log(name_1, job, live)
---
+++ assets/script/data-types.js
@@ -0,0 +1,73 @@
+// Primitive data types
+
+let numOne = 3
+let numTwo = 3
+
+console.log(numOne == numTwo)      // true
+
+let js = 'JavaScript'
+let py = 'Python'
+
+console.log(js == py)             //false 
+
+let lightOn = true
+let lightOff = false
+
+console.log(lightOn == lightOff) // false
+
+
+
+
+
+
+
+//Non-Primitive Data Types(References)
+
+let nums = [1, 2, 3]
+nums[0] = 10
+
+console.log(nums)  // [10, 2, 3]
+
+
+
+
+
+/*
+let nums = [1, 2, 3]
+let numbers = [1, 2, 3]
+
+console.log(nums == numbers)  // false
+
+let userOne = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+
+let userTwo = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+
+console.log(userOne == userTwo) // false
+
+
+*/
+
+//value-lari eyni referansi aldigi ucun bu defe dogru
+
+let nums = [1, 2, 3]
+let numbers = nums
+
+console.log(nums == numbers)  // true
+
+let userOne = {
+name:'Asabeneh',
+role:'teaching',
+country:'Finland'
+}
+
+let userTwo = userOne
+
+console.log(userOne == userTwo)  // true
