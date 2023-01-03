1d051827fbb669ec262a1ba2d33e166945770e6a
eshginfarzali <85007480+eshginfarzali@users.noreply.github.com>
2023-01-03 19:27:28 +0400

    math-

--- assets/script/app.js
+++ assets/script/app.js
@@ -21,11 +21,11 @@ console.log(firstName, lastName, country, city, age, isMarried)
 
 
 // Declaring variables with number values
-let ageYears= 100 // age in years
+/*let ageYears= 100 // age in years
 const gravity = 9.81 // earth gravity  in m/s2
 const boilingPoint = 100 // water boiling point, temperature in °C
 const PI = 3.14 // geometrical constant
-console.log(gravity, boilingPoint, PI)
+console.log(gravity, boilingPoint, PI)*/
 
 
 
--- assets/script/data-types.js
+++ assets/script/data-types.js
@@ -1,32 +1,34 @@
+
+
 // Primitive data types
 
-let numOne = 3
-let numTwo = 3
+/*let numOne = 3;
+let numTwo = 3;
 
-console.log(numOne == numTwo)      // true
+console.log(numOne == numTwo) ;     // true
 
-let js = 'JavaScript'
-let py = 'Python'
+let js = 'JavaScript';
+let py = 'Python';
 
-console.log(js == py)             //false 
+console.log(js == py)   ;          //false 
 
-let lightOn = true
-let lightOff = false
+let lightOn = true;
+let lightOff = false;
 
-console.log(lightOn == lightOff) // false
-
-
+console.log(lightOn == lightOff); // false
 
 
+
+*/
 
 
 
 //Non-Primitive Data Types(References)
 
-let nums = [1, 2, 3]
-nums[0] = 10
+// let nums = [1, 2, 3]
+// nums[0] = 10
 
-console.log(nums)  // [10, 2, 3]
+// console.log(nums)  // [10, 2, 3]
 
 
 
@@ -57,10 +59,10 @@ console.log(userOne == userTwo) // false
 
 //value-lari eyni referansi aldigi ucun bu defe dogru
 
-let nums = [1, 2, 3]
-let numbers = nums
+let nums = [1, 2, 3];
+let numbers = nums;
 
-console.log(nums == numbers)  // true
+console.log(nums == numbers) ; // true
 
 let userOne = {
 name:'Asabeneh',
@@ -71,3 +73,16 @@ country:'Finland'
 let userTwo = userOne
 
 console.log(userOne == userTwo)  // true
+
+
+
+// let age = 35
+const gravity = 9.81  // we use const for non-changing values, gravitational constant in  m/s2
+let mass = 72         // mass in Kilogram
+const PI = 3.14       // pi a geometrical constant
+
+// More Examples
+const boilingPoint = 100 // temperature in oC, boiling point of water which is a constant
+const bodyTemp = 37      // oC average human body temperature, which is a constant
+
+console.log(age, gravity, mass, PI, boilingPoint, bodyTemp)
